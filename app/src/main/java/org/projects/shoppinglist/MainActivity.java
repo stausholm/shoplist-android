eb009bb290714b2b6f4c4a26aea71a0c4f46a3fa
stausholm <muffix26@gmail.com>
2018-04-05 14:31:02 +0200

    snackbar and product class

--- app/src/main/java/org/projects/shoppinglist/MainActivity.java
+++ app/src/main/java/org/projects/shoppinglist/MainActivity.java
@@ -1,7 +1,9 @@
 package org.projects.shoppinglist;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -13,26 +15,57 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MyDialogFragment.OnPositiveListener {
+
+    Map<Integer, Product> savedCopies = new HashMap<>();
+
+
+    MyDialogFragment dialog;
+    Context context;
 
     private final int RESULT_CODE_PREFERENCES = 1;
 
-    ArrayAdapter<String> adapter;
+    ArrayAdapter<Product> adapter;
     ListView listView;
-    ArrayList<String> bag = new ArrayList<>();
+    ArrayList<Product> bag = new ArrayList<>();
 
     public ArrayAdapter getMyAdapter()
     {
         return adapter;
     }
 
+
+    //This method is the one we need to implement from the
+    //interface. It will be called when the user has clicked the
+    //positive button (yes button):
+    @Override
+    public void onPositiveClicked() {
+        //Do your update stuff here to the listview
+        //and the bag etc
+        //just to show how to get arguments from the bag.
+        //Toast toast = Toast.makeText(context, "positive button clicked", Toast.LENGTH_LONG);
+        //toast.show();
+        //bag.clear(); //here you can do stuff with the bag and
+        //adapter etc.
+
+
+        bag.clear();
+        listView.clearChoices();
+        adapter.notifyDataSetChanged();
+        Toast.makeText(this,"All items cleared", Toast.LENGTH_SHORT).show();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        context = this;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //Needed to get the toolbar to work on older versions
@@ -43,7 +76,7 @@ public class MainActivity extends AppCompatActivity {
         //do we have some saved state?
         if (savedInstanceState != null) {
             System.out.println("test");
-            ArrayList<String> savedBag = savedInstanceState.getStringArrayList("savedBag");
+            ArrayList<Product> savedBag = savedInstanceState.getParcelableArrayList("savedBag");
             if (savedBag != null) { //did we save something
                 System.out.println("test deeper");
                 bag = savedBag;
@@ -56,7 +89,7 @@ public class MainActivity extends AppCompatActivity {
         listView = findViewById(R.id.list);
         //here we create a new adapter linking the bag and the
         //listview
-        adapter =  new ArrayAdapter<>(this,
+        adapter =  new ArrayAdapter<Product>(this,
                 android.R.layout.simple_list_item_multiple_choice,bag );
 
         //setting the adapter on the listview
@@ -70,12 +103,18 @@ public class MainActivity extends AppCompatActivity {
 
         final EditText addText = findViewById(R.id.addText);
         final EditText addQty = findViewById(R.id.addQty);
+        final Spinner addSpinner = findViewById(R.id.addSpinner);
 
         Button addButton =  findViewById(R.id.addButton);
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                adapter.add(addText.getText().toString() + " x " + addQty.getText().toString());
+                String item = (String) addSpinner.getSelectedItem();
+                System.out.println("item is: " + item);
+                //adapter.add(addText.getText().toString() + " x " + addQty.getText().toString());
+                if (addText.length() > 0) {
+                   adapter.add(new Product(addText.getText().toString(), Integer.parseInt(addQty.getText().toString()), item));
+                }
                 Log.d("Bag","Items in back: "+bag.size());
             }
         });
@@ -93,7 +132,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
 		/* Here we put code now to save the state */
-        outState.putStringArrayList("savedBag", bag);
+        outState.putParcelableArrayList("savedBag", bag);
     }
 
 
@@ -133,20 +172,40 @@ public class MainActivity extends AppCompatActivity {
             for (int i = bag.size() -1; i > -1; i--) {
                 //positions.get(i);
                 if (positions.get(i)) {
+                    saveProductCopy(i, bag.get(i));
                     bag.remove(i);
                 }
             }
+
+
+
+
+            final View parent = listView;
+            Snackbar snackbar = Snackbar
+                    .make(parent, "Items Deleted", Snackbar.LENGTH_LONG)
+                    .setAction("UNDO", new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            readdDeletedProducts();
+                            adapter.notifyDataSetChanged();
+                        }
+                    });
+
+            if (positions.size() > 0) {
+                snackbar.show();
+            }
+
+
+
             listView.clearChoices();
             //bag.remove(positions);
             adapter.notifyDataSetChanged();
             //listView.setItemChecked(-1,true);
-            return true;
+            //return true;
         }
         if (id == R.id.action_clear) {
-            bag.clear();
-            adapter.notifyDataSetChanged();
-            Toast.makeText(this,"All items cleared", Toast.LENGTH_SHORT).show();
-            return true;
+            showDialog();
+
         }
 
         return super.onOptionsItemSelected(item);
@@ -179,5 +238,45 @@ public class MainActivity extends AppCompatActivity {
             updateUI(name,male);
         }
         super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    //This is the event handler for the show button
+    //This is specified in the xml file that this should
+    //be the event handler
+    public void showDialog() {
+        //showing our dialog.
+
+        dialog = new MyDialog();
+        //Here we show the dialog
+        //The tag "MyFragement" is not important for us.
+        dialog.show(getFragmentManager(), "MyFragment");
+    }
+
+    public static class  MyDialog extends MyDialogFragment {
+
+
+        @Override
+        protected void negativeClick() {
+            //Here we override the method and can now do something
+            Toast toast = Toast.makeText(getActivity(),
+                    "negative button clicked", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+
+
+    }
+
+    public void saveProductCopy(int position, Product product) {
+        savedCopies.put(position, product);
+        //readdDeletedProducts();
+    }
+
+    public void readdDeletedProducts() {
+        for (Map.Entry<Integer, Product> entry : savedCopies.entrySet()) {
+            int key = entry.getKey();
+            Product value = entry.getValue();
+            bag.add(key, value);
+        }
+        savedCopies.clear();
     }
 }
---
+++ app/src/main/java/org/projects/shoppinglist/Product.java
@@ -0,0 +1,60 @@
+package org.projects.shoppinglist;
+
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Product implements Parcelable {
+
+    String name;
+    int quantity;
+    String variant;
+
+    public Product() {} //Empty constructor we will need later!
+
+    public Product(String name, int quantity, String variant)
+    {
+        this.name = name;
+        this.quantity = quantity;
+        this.variant = variant;
+    }
+
+    @Override
+    public String toString() {
+        return name+" "+quantity+" "+variant;
+    }
+
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(name);
+        dest.writeInt(quantity);
+        dest.writeString(variant);
+    }
+
+    // Creator
+    public static final Parcelable.Creator CREATOR
+            = new Parcelable.Creator() {
+        public Product createFromParcel(Parcel in) {
+            return new Product(in);
+        }
+
+        public Product[] newArray(int size) {
+            return new Product[size];
+        }
+    };
+
+    // "De-parcel object
+    public Product(Parcel in) {
+        name = in.readString();
+        quantity = in.readInt();
+        variant = in.readString();
+    }
+
+}
+
