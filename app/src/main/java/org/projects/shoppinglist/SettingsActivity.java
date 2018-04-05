43edf9cd5fb090593087277172e4673671be1d91
stausholm <muffix26@gmail.com>
2018-04-05 09:32:40 +0200

    messing around with settings activity and fragments

--- app/src/main/java/org/projects/shoppinglist/SettingsActivity.java
+++ app/src/main/java/org/projects/shoppinglist/SettingsActivity.java
@@ -2,7 +2,13 @@ package org.projects.shoppinglist;
 
 
 import android.app.Activity;
+//import android.app.FragmentTransaction;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+//import android.support.v4.app.FragmentActivity;
+//import android.support.v4.app.FragmentTransaction;
 
 
 public class SettingsActivity extends Activity {
@@ -11,6 +17,7 @@ public class SettingsActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        //setContentView(R.layout.test_main);
         // Display the fragment as the main content.
         //note that the ID named "content" is defined by Android -
         //it is NOT an identifier we define in .xml
@@ -20,5 +27,25 @@ public class SettingsActivity extends Activity {
         //note - there is no setContentView and no xml layout
         //for this activity. Because that is defined 100 %
         //in the fragment (MyPreferencesFragment)
+        // Begin the transaction
+
+//        // Create new fragment and transaction
+//        Fragment newFragment = new MyPreferenceFragment();
+//        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+//
+//        // Replace whatever is in the fragment_container view with this fragment,
+//        // and add the transaction to the back stack if needed
+//        transaction.add(R.id.testFragment, newFragment);
+//        transaction.addToBackStack(null);
+//
+//        // Commit the transaction
+//        transaction.commit();
+
+//        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+//        // Replace the contents of the container with the new fragment
+//        ft.replace(R.id.your_placeholder, new MyPreferenceFragment());
+//        // or ft.add(R.id.your_placeholder, new FooFragment());
+//        // Complete the changes added above
+//        ft.commit();
     }
 }
